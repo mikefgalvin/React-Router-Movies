274004456e94240f33817e3e4cfad35be290108b
Mike <mgalvin@tribevr.io>
2020-12-14 14:52:19 -0600

    mvp done

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function MovieList(props) {
+
+  const { url } = useRouteMatch();
+  console.log('url console log', url);
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
+        <Link key={movie.id} to={`${url}movies/${movie.id}`}> 
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
@@ -15,13 +22,15 @@ function MovieDetails(props) {
 
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
+       
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+      
     </div>
   );
 }
